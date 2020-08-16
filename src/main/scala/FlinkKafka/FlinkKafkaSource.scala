c2776a30d2d2baa1cfa718c00daed7523d923f3e
luckychacha <358140284@qq.com>
2020-08-16 23:54:42 +0800

    flink kafka as source

--- src/main/scala/FlinkKafka/FlinkKafkaSource.scala
+++ src/main/scala/FlinkKafka/FlinkKafkaSource.scala
@@ -12,32 +12,6 @@ import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
 object FlinkKafkaSource {
 
   def main(args: Array[String]): Unit = {
-//    val env = StreamExecutionEnvironment.getExecutionEnvironment
-//    //隐式转换
-//    import org.apache.flink.api.scala._
-//    //checkpoint配置
-//    env.enableCheckpointing(100);
-//    env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
-//    env.getCheckpointConfig.setMinPauseBetweenCheckpoints(500);
-//    env.getCheckpointConfig.setCheckpointTimeout(60000);
-//    env.getCheckpointConfig.setMaxConcurrentCheckpoints(1);
-//    env.getCheckpointConfig.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
-//    val topic = "test"
-//
-//    val prop = new Properties()
-//    prop.setProperty("bootstrap.servers","node01:9092")
-//    prop.setProperty("group.id","con1")
-//    prop.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-//    prop.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-//
-//    var kafkaSoruce: FlinkKafkaConsumer011[String] = new FlinkKafkaConsumer011[String](topic, new SimpleStringSchema(), prop)
-//
-//    kafkaSoruce.setCommitOffsetsOnCheckpoints(true)
-//    //设置statebackend
-//    env.setStateBackend(new RocksDBStateBackend("hdfs://node01:8020/flink_kafka/checkpoints",true));
-//    val result: DataStream[String] = env.addSource(kafkaSoruce)
-//    result.print()
-//    env.execute()
 
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
     import org.apache.flink.api.scala._
