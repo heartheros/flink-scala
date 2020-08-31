d223cfa9b5d881ba1323883d6b48ee8df7fb3bbb
luckychacha <358140284@qq.com>
2020-08-31 23:04:29 +0800

    broadcast

---
+++ src/main/scala/DataSet/FlinkDataSetBroadCast.scala
@@ -0,0 +1,43 @@
+package DataSet
+
+import org.apache.flink.api.common.functions.RichMapFunction
+import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.configuration.Configuration
+
+import scala.collection.mutable
+import java.util
+
+object FlinkDataSetBroadCast {
+  def main(args: Array[String]): Unit = {
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+
+    import org.apache.flink.api.scala._
+    val productData: DataSet[String] = env.readTextFile("/Users/leixinxin/Downloads/product.txt")
+
+    val productDataMap = new mutable.HashMap[String, String]
+
+    val productDataMapSet: DataSet[mutable.HashMap[String, String]] = productData.map(x => {
+      productDataMap.put(x.split(",")(0), x)
+      productDataMap
+    })
+
+    val orderData: DataSet[String] = env.readTextFile("/Users/leixinxin/Downloads/orders.txt")
+    val res: DataSet[String] = orderData.map(new RichMapFunction[String, String] {
+      var dataList: util.List[Map[String, String]] = null
+      var allMap = Map[String, String]()
+      override def open(parameters: Configuration): Unit = {
+        dataList = getRuntimeContext.getBroadcastVariable[Map[String, String]]("ProductDataMapSet")
+        val listIterator: util.Iterator[Map[String, String]] = dataList.iterator()
+        while(listIterator.hasNext) {
+          allMap = allMap.++(listIterator.next())
+        }
+      }
+      override def map(order: String): String = {
+        order + allMap.getOrElse(order.split(",")(2), "暂时没有匹配的值")
+      }
+    }).withBroadcastSet(productDataMapSet, "ProductDataMapSet")
+
+    res.print()
+    env.execute("broadcast params")
+  }
+}
