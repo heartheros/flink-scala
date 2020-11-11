48515113f280f69f7face172bb1566c212c2969f
leixinxin <luckychacha@163.com>
2020-11-11 18:42:24 +0800

    async query

--- flink_real_time/src/main/scala/Retail/MysqlDimensionAsyncFunction.scala
+++ flink_real_time/src/main/scala/Retail/MysqlDimensionAsyncFunction.scala
@@ -6,10 +6,10 @@ import java.util.Properties
 
 import com.alibaba.fastjson.{JSON, JSONObject}
 import io.vertx.core.Promise.promise
-import io.vertx.core.json.JsonObject
+import io.vertx.core.json.{JsonArray, JsonObject}
 import io.vertx.lang.scala.VertxExecutionContext
-import io.vertx.scala.core.{Vertx, VertxOptions}
-import io.vertx.scala.ext.sql.SQLClient
+import io.vertx.scala.core.{Future, Promise, Vertx, VertxOptions}
+import io.vertx.scala.ext.sql.{ResultSet, SQLClient}
 
 import scala.util.{Failure, Success}
 //import io.vertx.ext.jdbc.JDBCClient
@@ -23,29 +23,49 @@ import scala.collection.mutable.ListBuffer
 
 class MysqlDimensionAsyncFunction extends RichAsyncFunction[BinLogObject, BinLogObject] {
 
-  var dbProps: Properties = _
+  var globalConfig: Properties = _
+  var sqlConfig: Properties = _
   var jedisCon: Jedis = _
   var sqlClient: SQLClient = _
-  var vertx: Vertx = _
-
+  val vertx: Vertx = Vertx.vertx(VertxOptions().setWorkerPoolSize(10).setEventLoopPoolSize(5))
+  val config: JsonObject = new JsonObject()
+    .put("driver_class", "com.mysql.jdbc.Driver")
+    .put("max_pool_size", 20)
+    .put("user", globalConfig.getProperty("mysql.user"))
+    .put("password", globalConfig.getProperty("mysql.password"))
   var dimensionTables: Set[String] = _
   var cacheTables: Set[String] = _
   var realTimeTables: Set[String] = _
 
-  override def open(parameters: Configuration): Unit = {
+  var aTypes: Set[String] = _
+  var bTypes: Set[String] = _
 
-    dbProps = new Properties()
-    dbProps.load(
+  override def open(parameters: Configuration): Unit = {
+    globalConfig = new Properties()
+    globalConfig.load(
       new FileInputStream(Thread.currentThread()
         .getContextClassLoader
         .getResource("config.properties")
         .getPath)
     )
-    dimensionTables = dbProps.getProperty("tables.dimension").split(",").toSet
-    cacheTables = dbProps.getProperty("tables.cache").split(",").toSet
-    realTimeTables = dbProps.getProperty("tables.realtime").split(",").toSet
 
-    jedisCon = new Jedis("redis.host", 6379)
+    dimensionTables = globalConfig.getProperty("tables.dimension").split(",").toSet
+    cacheTables = globalConfig.getProperty("tables.cache").split(",").toSet
+    realTimeTables = globalConfig.getProperty("tables.realtime").split(",").toSet
+
+    sqlConfig = new Properties()
+    sqlConfig.load(
+      new FileInputStream(Thread.currentThread()
+        .getContextClassLoader
+        .getResource("sql.properties")
+        .getPath))
+    aTypes = sqlConfig.getProperty("dimension.a.types").split(",").toSet
+    bTypes = sqlConfig.getProperty("dimension.b.types").split(",").toSet
+
+    jedisCon = new Jedis(
+      globalConfig.getProperty("redis.host"),
+      globalConfig.getProperty("redis.port").toInt
+    )
   }
 
   override def close(): Unit = {
@@ -66,64 +86,141 @@ class MysqlDimensionAsyncFunction extends RichAsyncFunction[BinLogObject, BinLog
     val line: JSONObject = JSON.parseObject(input.data)
 
     if (dimensionTables.contains(table) && !opType.equalsIgnoreCase("insert")) {
-      setRedisExpire(getRedisKey(database, table, line.get("id").toString.toInt))
+      setRedisExpire(getRedisKey(database, table, line.get("id").toString))
     }
 
     if (cacheTables.contains(table)) {
     }
 
     if (realTimeTables.contains(table)) {
+      val dimensionSql: String = getDimensionSql(database, table, line)
+      if (null != dimensionSql) {
+        genMysqlClient(input.database)
 
-      var dimensionSqlListBuffer: ListBuffer[String] = ListBuffer();
-      dimensionSqlListBuffer += getSql(
-        table,
-        line.get("aaa").toString.toInt,
-        "a,b".split(","),
-        "aaa-name"
-      )
-      dimensionSqlListBuffer += getSql(
-        table,
-        line.get("bbb").toString.toInt,
-        "c,d".split(","),
-        "bbb-name"
-      )
+        // 获取 id
+        val infoId: Future[Int] = infoQuery(line.getIntValue("id"))
 
-      genMysqlConn(input.database)
-      sqlClient.getConnectionFuture().onComplete{
-        case Success(result) => {
-          var connection = result
+        // 根据 id 获取品牌
+        val brandString: Future[String] = infoId.compose(infoId => brandQuery(infoId))
 
-          connection.queryFuture(dimensionSqlListBuffer.mkString(" union all ")).onComplete{
-            case Success(result) => {
-              print(result)
-              // Do something with results
-            }
-            case Failure(cause) => println("Failure")
-          }(VertxExecutionContext(vertx.getOrCreateContext()))
-        }
-        case Failure(cause) =>
-          println(s"$cause")
-      }(VertxExecutionContext(vertx.getOrCreateContext()))
+        // 根据品牌获取单位
+        val unitId: Future[Int] = brandString.compose(brandString => unitQuery(brandString))
+
+
+//        val testConnectionFuture = sqlClient.getConnectionFuture()
+
+//        testConnectionFuture.onComplete {
+//          case Success(result) => {
+//            var connection = result
+//
+//            connection.queryFuture(dimensionSql).onComplete {
+//              case Success(result) => {
+//                print(result)
+//                // Do something with results
+//              }
+//              case Failure(cause) => println("Failure")
+//            }(VertxExecutionContext(vertx.getOrCreateContext()))
+//          }
+//          case Failure(cause) =>
+//            println(s"$cause")
+//        }(VertxExecutionContext(vertx.getOrCreateContext()))
+      }
     }
+  }
+
+  def infoQuery(goodsId: Int): Future[Int] = {
+    val promise: Promise[Int] = Promise.promise()
+    sqlClient.getConnection(ar => {
+      if (ar.failed()) {
+        promise.fail(ar.cause())
+        return null
+      }
+      // 获取连接成功
+      var connection = ar.result()
+      connection.querySingle("select info", result => {
+        promise.complete(result.result().get.getInteger(0))
+      })
+    })
+    sqlClient.close()
+    promise.future()
+  }
+
+  def brandQuery(goodsId: Int): Future[String] = {
+    val promise: Promise[String] = Promise.promise()
+    if (goodsId == 1) {
+      sqlClient.getConnection(ar => {
+        if (ar.failed()) {
+          promise.fail(ar.cause())
+          return null
+        }
+        var connection = ar.result()
+        connection.querySingle("select brand", result => {
+          if (result.succeeded()) {
+            promise.complete(result.result().get.getString(0))
+          }
+        })
+      })
+      sqlClient.close()
+    } else {
+      promise.complete("asdadsa")
+    }
+
+    promise.future()
+  }
+
+  def unitQuery(goodsId: String): Future[Int] = {
+    val promise: Promise[Int] = Promise.promise()
+    sqlClient.getConnection(ar => {
+      if (ar.failed()) {
+        promise.fail(ar.cause())
+        return null
+      }
+      var connection = ar.result()
+      connection.querySingle("select unit", result => {
+        if (result.succeeded()) {
+          promise.complete(result.result().get.getInteger(0))
+        }
+      })
+    })
+    sqlClient.close()
+    promise.future()
+  }
+
+  def getDimensionSql(database: String, table: String, line: JSONObject): String = {
+    val typeName = sqlConfig.getProperty(table)
+    var dimensionSqlListBuffer: ListBuffer[String] = ListBuffer();
+
+    sqlConfig.getProperty("dimension." + typeName + ".types").split(",").foreach(dimensionTableName => {
+      val rawSql = sqlConfig.getProperty("dimension.r." + typeName + ".sql." + dimensionTableName)
+
+//      dimension.a.types=info,category,brand,unit,channel,channel-name,material,mwac,wac
+//      dimension.b.types=info,category,channel,channel-name,material,mwac,wac
+
+      val index = line.get("dimension.r." + typeName + ".sql." + dimensionTableName + ".index").toString
+      if (null == getRedisCache(getRedisKey(database, dimensionTableName, index))) {
+//        val sql: Unit =
+        dimensionSqlListBuffer += printf(rawSql, index).toString
+      }
+    })
+
+
+    if (0 == dimensionSqlListBuffer.size) {
+      return null
+    }
+    dimensionSqlListBuffer.mkString(" union all")
   }
 
   def getSql(table: String, index: Int, fields: Array[String], indexFieldName: String): String = {
     "select " + table + "as t," + fields.mkString(",") + " from " + table + " where " + indexFieldName + "=" + index
   }
 
-  def genMysqlConn(database: String): Unit = {
-    vertx = Vertx.vertx(VertxOptions().setWorkerPoolSize(10).setEventLoopPoolSize(5))
+  def genMysqlClient(database: String): Unit = {
 
-    val config: JsonObject = new JsonObject()
-      .put("driver_class", "com.mysql.jdbc.Driver")
-      .put("max_pool_size", 20)
-      .put("user", dbProps.getProperty("mysql.user"))
-      .put("password", dbProps.getProperty("mysql.password"))
-    config.put("url", dbProps.getProperty("mysql.url") + database)
+    config.put("url", globalConfig.getProperty("mysql.url") + database)
     sqlClient = JDBCClient.createShared(vertx, config, database)
   }
 
-  def getRedisCacheWithExpire(redisKey: String): String = {
+  def getRedisCache(redisKey: String): String = {
     jedisCon.get(redisKey)
   }
 
@@ -138,7 +235,7 @@ class MysqlDimensionAsyncFunction extends RichAsyncFunction[BinLogObject, BinLog
     jedisCon.expire(redisKey, -1)
   }
 
-  def getRedisKey(database: String, table: String, id: Int): String = {
-    database + ":" + table + ":" + id.toString
+  def getRedisKey(database: String, table: String, id: String): String = {
+    database + ":" + table + ":" + id
   }
 }
--- flink_real_time/src/main/scala/Retail/TestReadConfig.scala
+++ flink_real_time/src/main/scala/Retail/TestReadConfig.scala
@@ -3,33 +3,70 @@ package Retail
 import java.io.FileInputStream
 import java.util.Properties
 
+import scala.concurrent.Future
+import io.vertx.core.json.JsonObject
+import io.vertx.lang.scala.VertxExecutionContext
+import io.vertx.scala.core.{Vertx, VertxOptions}
+import io.vertx.scala.ext.jdbc.JDBCClient
+import io.vertx.scala.ext.sql.{ResultSet, SQLClient}
+import redis.clients.jedis.Jedis
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+
 /**
  * @author Leixinxin
  * @date 2020/11/9 3:42 PM
  */
 object TestReadConfig {
   def main(args: Array[String]): Unit = {
-    var dbProps = new Properties()
-    dbProps.load(
+    var globalConfig = new Properties()
+//    var sqlConfig: Properties = _
+//    var vertx: Vertx = _
+    globalConfig.load(
       new FileInputStream(Thread.currentThread()
         .getContextClassLoader
         .getResource("config.properties")
         .getPath)
     )
-    val a: String = dbProps.getProperty("test")
-    val items: Set[String] = a.split(",").toSet
+    val vertx = Vertx.vertx(VertxOptions().setWorkerPoolSize(10).setEventLoopPoolSize(5))
 
-    print(items)
-    if (items.contains("1")) {
-      print("has 1" + "!")
-    } else {
-      print("1 not exist")
-    }
+    val config: JsonObject = new JsonObject()
+      .put("max_pool_size", 20)
+      .put("driver_class", "com.mysql.jdbc.Driver")
+      .put("user", globalConfig.getProperty("mysql.user"))
+      .put("password", globalConfig.getProperty("mysql.password"))
+    config.put("url", globalConfig.getProperty("mysql.url"))
+    val sqlClient = JDBCClient.createShared(vertx, config)
 
-    if (items.contains("5")) {
-      print("has 5" + "!")
-    } else {
-      print("5 not exist")
-    }
+    sqlClient.getConnectionFuture().onComplete{
+      case Success(result) => {
+
+        var connection = result
+
+        connection.queryFuture("SELECT * FROM sss").onComplete{
+          case Success(result) => {
+
+            var rs = result
+            rs.getResults.foreach(line => {
+//              println(line.getString(1))
+              println(line)
+            })
+            connection.close()
+          }
+          case Failure(cause) => {
+            println(s"$cause")
+            connection.close()
+          }
+        }
+      }
+      case Failure(cause) => {
+        println(s"$cause")
+      }
+    }(VertxExecutionContext(vertx.getOrCreateContext()))
+
+
+
+
   }
 }
