f4b390492a5017a54eccd3992c6c989be619d2d7
luckychacha <358140284@qq.com>
2020-11-23 00:37:41 +0800

    serialize

---
+++ flink_real_time/src/main/java/serialize/Payment.java
@@ -0,0 +1,15 @@
+package serialize;
+
+import org.phprpc.util.AssocArray;
+
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+/**
+ * @author luckychacha
+ * @date 2020-11-21 23:49
+ */
+public class Payment {
+
+}
---
+++ flink_real_time/src/main/scala/Retail/TestPHPSerialize.scala
@@ -0,0 +1,16 @@
+package Retail
+
+import java.util
+
+import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.serializer.SerializerFeature
+import org.phprpc.util.{AssocArray, PHPSerializer}
+import serialize.Payment
+
+import scala.beans.BeanProperty
+import scala.collection.immutable.HashMap
+
+object TestPHPSerialize {
+  def main(args: Array[String]): Unit = {
+  }
+}
